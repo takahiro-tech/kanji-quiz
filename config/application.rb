3a2eb0e761a73b2bd09a49a179a176f53902037f
takahiro-tech <56814714+takahiro-tech@users.noreply.github.com>
2020-01-10 22:36:13 +0900

    edit_error_messages

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,5 @@ module KanjiQuiz
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
-
-    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      if instance.kind_of?(ActionView::Helpers::Tags::Label)
-        # skip when label
-        html_tag.html_safe
-      else
-        "<div class=\"has-error\">#{html_tag}<span class=\"help-block\">#{instance.error_message.first}</span></div>".html_safe
-      end
-    end
   end
 end
