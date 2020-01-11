c267dbdfb137d30811ac12d64749aac23c9b9619
takahiro-tech <56814714+takahiro-tech@users.noreply.github.com>
2020-01-11 17:51:50 +0900

    edit devise

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,7 @@ module KanjiQuiz
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Asia/Tokyo'
-    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      if instance.kind_of?(ActionView::Helpers::Tags::Label)
-        # skip when label
-        html_tag.html_safe
-      else
-        "<div class=\"has-error\">#{html_tag}<span class=\"help-block\">#{instance.error_message.first}</span></div>".html_safe
-      end
-    end
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
